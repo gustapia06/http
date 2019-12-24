88401f550c44b6c0072e95d5bb65398293f7e45d
Gustavo Tapia <gustapia06@hotmail.com>
2019-12-24 13:20:58 -0800

    Initial commit

---
+++ src/main/java/restservice/TestRestService.java
@@ -0,0 +1,54 @@
+package restservice;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+public class TestRestService extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Get method called");
+        out.println("parameters: " + parameters(request));
+        out.println("headers: " + headers(request));
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Get method called");
+        out.println("parameters: " + parameters(request));
+        out.println("headers: " + headers(request));
+    }
+
+    public void doDelete(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Delete method called");
+    }
+
+    private String parameters(HttpServletRequest request) {
+        StringBuilder builder = new StringBuilder();
+        for (Enumeration<String> e = request.getParameterNames(); e.hasMoreElements(); ) {
+            String name = e.nextElement();
+            builder.append("|").append(name).append("->").append(request.getParameter(name));
+        }
+        return builder.toString();
+    }
+
+    private String headers(HttpServletRequest request) {
+        StringBuilder builder = new StringBuilder();
+        for (Enumeration<String> e = request.getHeaderNames(); e.hasMoreElements(); ) {
+            String name = e.nextElement();
+            builder.append("|").append(name).append("->").append(request.getHeader(name));
+        }
+        return builder.toString();
+    }
+}
